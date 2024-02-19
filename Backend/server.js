3097ccdf4aaf382da3febccc859953e2ec539906
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-02-19 23:09:17 +0530

    setting up router

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,12 @@
 require("dotenv").config()
 const express = require("express");
+const Authrouter = require('./routes/authRouter')
 const app = express();
 const PORT = process.env.PORT||3000;
 
-app.use(express.json())
+app.use(express.json());
+
+app.use('/Medico', Authrouter)
 app.get('/',(req,res)=>{
     res.send("hi")
 })
