49e87372121a8121a57e029c26dc1ea03c01f5bb
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-02-20 12:39:07 +0530

    signin route for name and password with status code

--- Backend/contoller/authcontoller.js
+++ Backend/contoller/authcontoller.js
@@ -1,6 +1,19 @@
-const{signin} = require("../routes/authRouter")
-const express = require('express')
-const router = express.Router()
 
-router.post( '/',signin)
-module.exports = router
+const signin = (req,res)=>{
+    const {name,password} = req.body
+    if(!name||!password){
+        res.status(400).json({
+            message: "Name and password are required"
+        })
+    }
+    if(name ==="SHRIVIDYACM" && password === "hello1"){
+        res.status(200).json({
+            message:"user loggined"
+        })
+    }
+    res.send("hi")
+}
+
+module.exports = {
+    signin
+}
