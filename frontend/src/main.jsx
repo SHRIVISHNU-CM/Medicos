40322f01cdb7961e0866c5e4352ca3cba2cccac4
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-02-22 22:52:57 +0530

    creating routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,34 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider ,createBrowserRouter} from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './Components/Home.jsx'
+import PatientArticle from './Components/PatientArticle.jsx'
+import About from './Components/About.jsx'
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<Layout/>,
+    children:[
+      {
+        path:"",
+        element:<Home/>
+      },{
+        path:"history",
+        element:<search/>
+      },
+      {
+        path:"article",
+        element:<PatientArticle/>
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/> 
   </React.StrictMode>,
 )
