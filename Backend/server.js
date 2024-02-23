50f0d5221bb1652a9dfec7672574fefa65c7d9b2
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-02-23 21:16:09 +0530

    setting cors

--- Backend/server.js
+++ Backend/server.js
@@ -2,11 +2,13 @@ require("dotenv").config()
 const express = require("express");
 const mongoData = require("./config/dataconnection")
 const Authrouter = require('./routes/authRouter')
+const cors = require("cors");
 const app = express();
 mongoData()
 const PORT = process.env.PORT||3000;
 
 app.use(express.json());
+app.use(cors())
 app.use(express.urlencoded({extended:false}))
 
 app.use('/Medico', Authrouter)
