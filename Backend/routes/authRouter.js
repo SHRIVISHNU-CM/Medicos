0d557259bcdfa0f871f5a926515e30aba64eedb0
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-02-23 11:42:59 +0530

    newarticle and editarticle routes

--- Backend/routes/authRouter.js
+++ Backend/routes/authRouter.js
@@ -1,6 +1,8 @@
-const { signin } = require("../contoller/authcontoller")
+const { signin,article ,editarticle} = require("../contoller/authcontoller")
 const express = require('express')
 const router = express.Router()
 
 router.post('/signin', signin)
+router.post('/newarticle',article)
+router.put('/editarticle/:id',editarticle)
 module.exports = router
