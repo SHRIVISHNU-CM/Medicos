a6312be2e80794cafb2583d2bf79f94cf84a373e
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-02-23 13:19:29 +0530

    delete route

--- Backend/routes/authRouter.js
+++ Backend/routes/authRouter.js
@@ -1,8 +1,9 @@
-const { signin,article ,editarticle} = require("../contoller/authcontoller")
+const { signin,article ,editarticle, deleteArticle} = require("../contoller/authcontoller")
 const express = require('express')
 const router = express.Router()
 
 router.post('/signin', signin)
 router.post('/newarticle',article)
-router.put('/editarticle/:id',editarticle)
+router.put('/editarticle/:id',editarticle);
+router.delete('/deletearticle/:id',deleteArticle)
 module.exports = router
