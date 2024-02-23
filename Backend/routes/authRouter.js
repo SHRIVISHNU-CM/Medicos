5b52d791162be9f525ea302b4be556112e7b134f
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-02-23 21:17:22 +0530

    routes for getDetails and findOne

--- Backend/routes/authRouter.js
+++ Backend/routes/authRouter.js
@@ -1,8 +1,10 @@
-const { signin,article ,editarticle, deleteArticle} = require("../contoller/authcontoller")
+const { signin,article ,editarticle, deleteArticle, AllDetails,GetDetailsFromData} = require("../contoller/authcontoller")
 const express = require('express')
 const router = express.Router()
 
 router.post('/signin', signin)
+router.get('/',AllDetails)
+router.get('/:id',GetDetailsFromData)
 router.post('/newarticle',article)
 router.put('/editarticle/:id',editarticle);
 router.delete('/deletearticle/:id',deleteArticle)
