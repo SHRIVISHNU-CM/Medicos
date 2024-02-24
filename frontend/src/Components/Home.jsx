fc6403b38fc6d4c97db9b1ccc89c1d2533b5c0c7
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-02-24 20:31:24 +0530

    deleting view

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -2,7 +2,6 @@ import { useState, useEffect } from "react";
 import { FaLocationDot } from "react-icons/fa6";
 import { IoIosCall } from "react-icons/io";
 import { FaArrowRight } from "react-icons/fa";
-import View from "./ViewButton/View";
 import axios from "axios";
 function Home() {
     const [Data, SetData] = useState([]);
