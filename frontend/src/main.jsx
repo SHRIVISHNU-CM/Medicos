8914fe62798fd7383b123742860b0711eccef9bb
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-02-24 20:29:39 +0530

    changing alternative router method

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,35 +2,46 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { RouterProvider ,createBrowserRouter} from 'react-router-dom'
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom'
 import Layout from './Layout.jsx'
 import Home from './Components/Home.jsx'
 import PatientArticle from './Components/PatientArticle.jsx'
 import Search from './Components/Search.jsx'
 
 
-const router = createBrowserRouter([
-  {
-    path:'/',
-    element:<Layout/>,
-    children:[
-      {
-        path:"",
-        element:<Home/>
-      },{
-        path:"search",
-        element:<Search/>
-      },
-      {
-        path:"article",
-        element:<PatientArticle/>
-      }
-    ]
-  }
-])
+
+// const router = createBrowserRouter([
+//   {
+//     path:'/',
+//     element:<Layout/>,
+//     children:[
+//       {
+//         path:"",
+//         element:<Home/>,
+
+//       },{
+//         path:"search",
+//         element:<Search/>
+//       },
+//       {
+//         path:"article",
+//         element:<PatientArticle/>
+//       }
+//     ]
+//   }
+// ])
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path='search' element={<Search/>}/>
+      <Route path='article' element={<PatientArticle/>}/>
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}/> 
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
