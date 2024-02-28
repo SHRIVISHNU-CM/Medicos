ee09f6dd45e105ed680c64f701a9abedca042ed4
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-02-28 08:51:06 +0530

    routes for get data by _id

--- Backend/routes/authRouter.js
+++ Backend/routes/authRouter.js
@@ -1,10 +1,11 @@
-const { signin,article ,editarticle, deleteArticle, AllDetails,GetDetailsFromData} = require("../contoller/authcontoller")
+const { signin,article ,editarticle, deleteArticle, AllDetails,GetDetailsFromData,GetItmesById} = require("../contoller/authcontoller")
 const express = require('express')
 const router = express.Router()
 
 router.post('/signin', signin)
 router.get('/',AllDetails)
 router.get('/:id',GetDetailsFromData)
+router.get('/byid/:id',GetItmesById)
 router.post('/newarticle',article)
 router.put('/editarticle/:id',editarticle);
 router.delete('/deletearticle/:id',deleteArticle)
