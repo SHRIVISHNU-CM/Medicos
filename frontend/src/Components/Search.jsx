6a830404bd570745a2e6d450e68aeeefa9d24b30
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-02-28 14:25:53 +0530

    adding UI's to page

--- frontend/src/Components/Search.jsx
+++ frontend/src/Components/Search.jsx
@@ -1,17 +1,23 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
 
 function Search() {
-    const [Data, SetData] = useState()
-    const [SearchData, SetSearchData] = useState({})
-    const URI = `http://localhost:3001/Medico/${Data}`
-    const handleSearch =  () => {
+    const [Data, SetData] = useState("")
+    const [SearchData, SetSearchData] = useState(null)
+    const [idData, setidData] = useState("")
+    const URI = `http://localhost:3001/Medico/${Data}`;
+
+
+    const handleSearch = () => {
         try {
 
-             axios.get(URI).then((res)=>{
+            axios.get(URI).then((res) => {
+                setidData(res.data._id)
                 SetSearchData(res.data)
-                console.log(res.data.name)
-             })
+                setFormData(res.data)
+                console.log(res.data._id)
+            })
         } catch (error) {
             console.error("Error occurred:", error);
         }
@@ -22,10 +28,12 @@ function Search() {
         if (Data) {
             handleSearch();
         }
-    }, [Data]);
+    }, []);
+
 
     return (
         <>
+
             <div className='flex justify-center gap-3 my-2'>
                 <input
                     value={Data}
@@ -40,14 +48,48 @@ function Search() {
                     onClick={handleSearch}
 
                 >Search</button>
-            </div>
-            <p>{SearchData.name}</p>
-           <h2>{SearchData.history}</h2>
-            {/* <h1>{SearchData.name}</h1>
-            <h2>{SearchData.patient_id}</h2>
-            <h1>{SearchData.history}</h1>
-            <p>{SearchData.medicine_given}</p> */}
 
+            </div>
+            {
+                SearchData !== null && (
+                    <>
+                        <div className="w-full h-min p-1 flex justify-center">
+                            <header className="px-2 py-1 w-[400px] h-min">
+                                <div className="border border-b-blue-600 p-1 my-1">
+                                    <h1>Patient Name:</h1>
+                                    <p>{SearchData.name}</p>
+                                </div>
+
+                                <div className="border border-b-blue-600 p-1 my-1">
+                                    <h1>Mobile Number:</h1>
+                                    <p>{SearchData.phoneno}</p>
+
+                                </div>
+                                <div className="border border-b-blue-600 p-1 my-1">
+                                    <h1>Location:</h1>
+                                    <p>{SearchData.location}</p>
+
+                                </div>
+                                <div className="border border-b-blue-600 p-1 w-[384px]  h-min my-1">
+                                    <h1>Medicine:</h1>
+                                    <p>{SearchData.medicine_given}</p>
+
+                                </div>
+                                <div className="border border-b-blue-600 p-1 w-[384px]  h-min my-1">
+                                    <h1>History:</h1>
+                                    <p>{SearchData.history}</p>
+
+                                </div>
+                            </header>
+
+
+                        </div>
+                        <Link to={`/edit/${idData}`} className="border px-4 py-2 bg-green-700 text-white m-1 w-[200px] block mx-auto text-center rounded-md">
+                            Edit
+                        </Link>
+
+                    </>)
+            }
         </>
     )
 }
