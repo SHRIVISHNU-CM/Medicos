3289f7c8b256246627a282efa01728b9ca29c399
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-02-28 14:25:12 +0530

    functionality for logo

--- frontend/src/Components/Nav.jsx
+++ frontend/src/Components/Nav.jsx
@@ -3,7 +3,8 @@ function Navbar() {
     return (
         <>
             <navbar className="border flex justify-between items-center p-2 border-blue-800 shadow-2xl">
-                <h1 className="text-2xl text-blue-600">Shrividya's HomeoCare</h1>
+            <Link className="text-2xl text-blue-600">Shrividya's HomeoCare</Link>
+                
                 <div className="flex gap-2">
                     <NavLink to="/" className={({ isActive }) => `${isActive ? "text-orange-500" : "text-gray-500"}`
 
