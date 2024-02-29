375bb40a84c9b9b0cb9142b1447c3c0e73530e6e
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-02-29 23:09:33 +0530

    Navlink for filterSearch

--- frontend/src/Components/Nav.jsx
+++ frontend/src/Components/Nav.jsx
@@ -2,24 +2,30 @@ import { NavLink, Link } from "react-router-dom"
 function Navbar() {
     return (
         <>
-            <navbar className=" flex justify-between items-center p-2 shadow-lg">
-            <Link className="text-2xl text-blue-600">Shrividya's HomeoCare</Link>
-                
-                <div className="flex gap-2">
-                    <NavLink to="/" className={({ isActive }) => `${isActive ? "text-orange-500" : "text-gray-500"}`
-
-                    }>
-                        Home
-                    </NavLink>
-
-                    <NavLink to="/search"
-                        className={({ isActive }) => `${isActive ? "text-orange-500" : "text-gray-500"}`}
-                    >Search</NavLink>
-                    <NavLink to="/article" className={({ isActive }) => `${isActive ? "text-orange-500" : "text-gray-500"}`}>
-                        New Record</NavLink>
-
-                </div>
+            <navbar className="w-full flex justify-center">
+                <Link className="font-bold text-green-800 text-lg"
+                 to="/">Shrividya's HomeoCare</Link>
             </navbar >
+
+            <div className="flex gap-2 justify-around shadow-lg w-full">
+                <NavLink to="/"
+                    className={({ isActive }) => `${isActive ? "text-orange-500" : "text-gray-500"}`
+                    }>
+                    Home
+                </NavLink>
+
+                <NavLink to="/search"
+                    className={({ isActive }) => `${isActive ? "text-orange-500" : "text-gray-500"}`}
+                >Search</NavLink>
+                <NavLink to="/filter"
+                    className={({ isActive }) => `${isActive ? "text-orange-500" : "text-gray-500"}`}
+                >Filter</NavLink>
+                <NavLink to="/article" className={({ isActive }) => `${isActive ? "text-orange-500" : "text-gray-500"}`}>
+                    New</NavLink>
+
+            </div>
+
+
         </>
     )
 }
