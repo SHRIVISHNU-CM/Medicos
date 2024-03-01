eff04ed74617298fa7bd8db9e8e0247d5eda5604
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-03-01 19:08:19 +0530

    filter data based on input

--- frontend/src/Components/Filter/NameSearch.jsx
+++ frontend/src/Components/Filter/NameSearch.jsx
@@ -1,13 +1,38 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import axios from "axios";
 function NameSearch() {
+  const [Data, SetData] = useState("")
+  const[records ,SetRecords] = useState([])
+  const API = "http://localhost:3001/Medico/"
+  useEffect(() => {
+    axios.get(API)
+      .then((res) => {
+        SetData(res.data)
+        SetRecords(res.data)
+        console.log(res)
+      })
+  }, [])
+
+
   return (
     <>
       <div>
         <input
           type='text'
+          onChange={
+                (e)=>{
+                  SetRecords(Data.filter(f=>f.name.toLowerCase().includes(e.target.value)))
+                }
+          }
 
         />
+      </div>
+      <div>
+        {
+          records.map((el,i)=>{
+            return(<li key={i}>{el.name}</li>)
+          })
+        }
       </div>
     </>
   )
