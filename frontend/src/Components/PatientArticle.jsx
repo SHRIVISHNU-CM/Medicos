885abc31aa84d9dbf288c6a80b0bde01897aba30
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-03-03 00:05:20 +0530

    handling error

--- frontend/src/Components/PatientArticle.jsx
+++ frontend/src/Components/PatientArticle.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import axios from "axios"
 import { useNavigate } from "react-router-dom";
 import { MdCreateNewFolder } from "react-icons/md";
-import {toast} from "react-toastify"
+import { toast } from "react-toastify"
 function PatientArticle() {
    const Navigate = useNavigate()
    const [name, SetName] = useState("")
@@ -17,7 +17,7 @@ function PatientArticle() {
    const URI = "http://localhost:3001/Medico/newarticle";
    const handleCreate = async () => {
       try {
-         const result = await axios.post(URI, { name, patient_id, history, medicine_given, phoneno, location,age,gender,address })
+         const result = await axios.post(URI, { name, patient_id, history, medicine_given, phoneno, location, age, gender, address })
          console.log(result)
 
       } catch (e) {
@@ -27,57 +27,65 @@ function PatientArticle() {
    }
    const handleSubmit = async (e) => {
       e.preventDefault()
-      handleCreate()
-      toast.success(`Created id ${patient_id}`)
+      if (!name || !patient_id || !history || !medicine_given || !phoneno || !location || !age || !gender || !address) {
+         toast.warn("All Fields are required")
+      } else {
+         handleCreate()
+         toast.success(`Created id ${patient_id}`)
+         Navigate('/search')
+      }
 
-      Navigate('/search')
    }
 
    return (
       <>
-         <div className="border w-[450px] h-min bg-slate-600 block mx-auto my-1">
-            <form className="flex items-center flex-col" onSubmit={handleSubmit}>
-               <div><label htmlFor="Name" className="text-white font-semibold text-lg">Name</label></div>
+         <div className="border w-[450px] h-min  block mx-auto my-1 shadow-2xl">
+            <form className="px-2 py-1" onSubmit={handleSubmit}>
+               <div><label htmlFor="Name" className=" font-semibold text-slate-600 text-lg">Name</label></div>
                <input
-                  className="p-2 outline-none border border-blue-500 rounded-md w-62"
+                  className="p-2 border outline-blue-500 rounded-md w-62 w-full"
                   value={name}
                   onChange={(e) => {
                      console.log(e.target.value)
                      SetName(e.target.value)
                   }}
                   type="text" />
-               <div><label className="text-white font-semibold text-lg">Patient_id:</label></div>
+               <div><label className="w-full font-semibold text-lg">Patient_id:</label></div>
                <input
                   value={patient_id}
                   onChange={(e) => {
                      console.log(e.target.value)
                      SetPatient_id(e.target.value)
                   }}
-                  className="p-2 outline-none border border-blue-500 rounded-md w-62"
+                  className="p-2  border outline-blue-500 rounded-md w-62"
                   type="string" />
 
 
 
-               <div><label htmlFor="Phone Number" className="text-white font-semibold text-lg">Phone Number:</label></div>
+               <div><label htmlFor="Phone Number" className=" font-semibold text-lg ">
+                  Phone Number:
+               </label>
+               </div>
                <input
                   value={phoneno}
                   onChange={(e) => {
                      console.log(e.target.value)
                      SetPhone(e.target.value)
                   }}
-                  className="p-2 outline-none border border-blue-500 rounded-md w-62"
+                  className="p-2 w-full border outline-blue-500 rounded-md w-62 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
                   type="Number" />
-               <div><label className="text-white font-semibold text-lg">Location</label></div>
+               <div><label className=" font-semibold text-lg">Location</label></div>
                <input
                   value={location}
                   onChange={(e) => {
                      console.log(e.target.value)
                      SetLocation(e.target.value)
                   }}
-                  className="p-2 outline-none border border-blue-500 rounded-md w-62"
+                  className="p-2 w-full border outline-blue-500 rounded-md w-62"
                   type="text" />
-               <div><label className="text-white font-semibold text-lg">DOB:</label></div>
+               <div><label className="font-semibold text-lg">DOB:</label></div>
                <input
+                  className="p-2 w-full border outline-blue-500 rounded-md w-62"
                   type="date"
                   value={age}
                   onChange={(e) => {
@@ -85,33 +93,34 @@ function PatientArticle() {
                      console.log(e.target.value)
                   }}
                />
-               <div><label className="text-white font-semibold text-lg">Gender:</label></div>
+               <div><label className=" font-semibold text-lg">Gender:</label></div>
                <input
+                  className="p-2 w-full border outline-blue-500 rounded-md w-62"
                   type="text"
                   value={gender}
                   onChange={(e) => {
                      setGender(e.target.value)
                      console.log(e.target.value)
                   }} />
-               <div><label className="text-white font-semibold text-lg">Residential Address</label></div>
+               <div><label className="font-semibold text-lg">Residential Address</label></div>
                <textarea
-               className="border w-[350px] rounded-md p-1 outline-none"
+                  className="border w-full rounded-md p-1 outline-blue-500"
                   value={address}
                   onChange={(e) => {
                      setAddress(e.target.value)
                      console.log(e.target.value)
                   }}
                />
-               <div><label className="text-white font-semibold text-lg">Patient's case</label></div>
-               <textarea className="border w-[350px] rounded-md p-1 outline-none"
+               <div><label className=" font-semibold text-lg">Patient's case</label></div>
+               <textarea className="border w-full rounded-md p-1 outline-blue-500"
                   value={history}
                   onChange={(e) => {
                      console.log(e.target.value)
                      SetCase(e.target.value)
                   }}
                   readOnly={false} />
-               <div><label className="text-white font-semibold text-lg">Medicine Given</label></div>
-               <textarea className="border w-[350px] rounded-md p-1 outline-none"
+               <div><label className="font-semibold text-lg">Medicine Given</label></div>
+               <textarea className="border w-full rounded-md p-1 outline-blue-500"
                   value={medicine_given}
                   onChange={(e) => {
                      console.log(e.target.value)
@@ -119,7 +128,7 @@ function PatientArticle() {
                   }}
                   readOnly={false} />
 
-               <button type="submit" className=" bg-red-500 px-4 py-2 rounded-md m-2 text-white flex items-center gap-1">
+               <button type="submit" className="text-white bg-red-500 px-4 py-2 rounded-md m-2  flex items-center gap-1">
                   Create
                   <MdCreateNewFolder />
                </button>
