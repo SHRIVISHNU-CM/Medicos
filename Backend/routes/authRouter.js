aff0d83b7bca6d596f693b9dc7dc1680235a0a1f
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-03-04 08:53:09 +0530

    route for signup

--- Backend/routes/authRouter.js
+++ Backend/routes/authRouter.js
@@ -1,8 +1,9 @@
-const { signin,article ,editarticle, deleteArticle, AllDetails,GetDetailsFromData,GetItmesById} = require("../contoller/authcontoller")
+const { signin,signup,article ,editarticle, deleteArticle, AllDetails,GetDetailsFromData,GetItmesById} = require("../contoller/authcontoller")
 const express = require('express')
 const router = express.Router()
 
 router.post('/signin', signin)
+router.post('/signup',signup)
 router.get('/',AllDetails)
 router.get('/:id',GetDetailsFromData)
 router.get('/byid/:id',GetItmesById)
