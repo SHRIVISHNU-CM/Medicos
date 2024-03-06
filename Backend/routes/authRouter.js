7c5cbb0559906552186ae815791253f104894df8
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-03-06 12:24:30 +0530

    middleware and logout route

--- Backend/routes/authRouter.js
+++ Backend/routes/authRouter.js
@@ -1,13 +1,15 @@
-const { signin,signup,article ,editarticle, deleteArticle, AllDetails,GetDetailsFromData,GetItmesById} = require("../contoller/authcontoller")
+const { signin,signup,logout,article ,editarticle, deleteArticle, AllDetails,GetDetailsFromData,GetItmesById} = require("../contoller/authcontoller")
+const {verifySignin,verifytoken,renewtoken} = require('../MIddleware/authmiddleware')
 const express = require('express')
 const router = express.Router()
 
-router.post('/signin', signin)
+router.post('/signin',[verifySignin], signin)
 router.post('/signup',signup)
-router.get('/',AllDetails)
-router.get('/:id',GetDetailsFromData)
-router.get('/byid/:id',GetItmesById)
-router.post('/newarticle',article)
-router.put('/editarticle/:id',editarticle);
-router.delete('/deletearticle/:id',deleteArticle)
+router.get('/logout',[verifytoken],logout)
+router.get('/',[verifytoken],AllDetails)
+router.get('/:id',[verifytoken],GetDetailsFromData)
+router.get('/byid/:id',[verifytoken],GetItmesById)
+router.post('/newarticle',[verifytoken],article)
+router.put('/editarticle/:id',[verifytoken],editarticle);
+router.delete('/deletearticle/:id',[verifytoken],deleteArticle)
 module.exports = router
