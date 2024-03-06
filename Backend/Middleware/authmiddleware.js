4979074f642b8ee6c8e3798b7c25829b81835afd
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-03-06 12:21:58 +0530

    verify token

---
+++ Backend/Middleware/authmiddleware.js
@@ -0,0 +1,49 @@
+const jwt = require('jsonwebtoken');
+const secret = "vishnu"
+const user_model = require('../config/UserModel')
+
+const verifySignin = async (req, res, next) => {
+    try {
+        if (!req.body.name) {
+            res.status(400).send({
+                message: "Enter Correct username"
+            })
+        }
+        if (!req.body.password) {
+            res.status(400).json({ message: "enter verify Password" })
+            console
+        }
+        next()
+    } catch (e) {
+        console.log(e)
+        res.status(400).json({ message: "error in page" })
+    }
+}
+
+const verifytoken = (req, res, next) => {
+    const token = req.cookies.accesstoken
+    if (!token) {
+    res.status(400).json({
+            success: false,
+            messsage: "invalid credentials"
+        })
+    } else {
+        jwt.verify(token,process.env.SECRET,(err,decoded)=>{
+            if(err){
+                res.json({
+                    message:"Error"
+                })
+            }else{
+                req.name= decoded.name;
+                next()
+            }
+        })
+    }
+
+}
+
+
+module.exports = {
+    verifySignin,
+    verifytoken,
+}
