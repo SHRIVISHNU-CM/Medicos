51263b73f8e72dd198e29cd7bedadc9e0604d4b8
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-03-10 11:32:29 +0530

    vercel

--- Backend/index.js
+++ Backend/index.js
@@ -12,10 +12,6 @@ app.use(express.json());
 app.use(cookieparser())
 app.use(cors())
 app.use(express.urlencoded({extended:false}))
-app.get("/", (req, res) => {
-    app.use(express.static(path.resolve(__dirname, "frontend", "build")));
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  })
 app.use('/Medico', Authrouter)
 
 app.get('/',(req,res)=>{
