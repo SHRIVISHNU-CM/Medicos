4a38994e61a070f0182041a132fcc58d375504fd
SHRIVISHNU-CM <cmshrivishnu@gmail.com>
2024-03-16 02:05:37 +0530

    vercel link

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cookieparser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://medicos-frontend.vercel.app",
     credentials:true,
     methods:["POST","PUT","DELETE","GET","PATCH","OPTIONS"]
 }
